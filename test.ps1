f3fa8f755f5b15e5c9ff0aecf139233a386e9a72
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-09 18:22:29 +0200

    premiere conditions

--- test.ps1
+++ test.ps1
@@ -1,3 +1,8 @@
 echo "On va faire un test"
-$val = Read-Host "Rentrer une chaîne de caractères"
-echo $val
+$val = Read-Host "Rentrer une chaine de caracteres"
+If($val -eq "bjr") 
+{ 
+  echo "Bienvenue !"
+}else{
+  echo "tu est mechant tu me dit pas bjr !!!"
+}
